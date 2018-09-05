d2e5ae5a1ac53cab5bdb988529c73e0b57ac9c70
Loo Rong Jie <loorongjie@gmail.com>
2018-09-05 08:25:36 +0800

    [CMake] Only include test targets if ABSL_RUN_TESTS=ON

--- CMake/AbseilHelpers.cmake
+++ CMake/AbseilHelpers.cmake
@@ -135,7 +135,7 @@ function(absl_test)
   )
 
 
-  if(BUILD_TESTING)
+  if(ABSL_RUN_TESTS)
 
     set(_NAME ${ABSL_TEST_TARGET})
     string(TOUPPER ${_NAME} _UPPER_NAME)
@@ -153,7 +153,7 @@ function(absl_test)
     set_property(TARGET ${_NAME}_bin PROPERTY FOLDER ${ABSL_IDE_FOLDER})
 
     add_test(${_NAME} ${_NAME}_bin)
-  endif(BUILD_TESTING)
+  endif(ABSL_RUN_TESTS)
 
 endfunction()
 
