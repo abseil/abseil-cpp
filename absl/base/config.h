7b4f80c60eba22af8cddec88e305181b387eb199
Pascal Leroy <phl+personal@google.com>
2018-10-26 20:31:22 +0200

    More properties.

--- absl/base/config.h
+++ absl/base/config.h
@@ -416,11 +416,15 @@
 // version.
 // TODO(zhangxy): fix tests before enabling aliasing for `std::any`.
 #if defined(_MSC_VER) && _MSC_VER >= 1910 && \
-    ((defined(_MSVC_LANG) && _MSVC_LANG > 201402) || __cplusplus > 201402)
+    ((defined(_MSVC_LANG) && _MSVC_LANG >= 201402) || __cplusplus > 201402)
 // #define ABSL_HAVE_STD_ANY 1
 #define ABSL_HAVE_STD_OPTIONAL 1
 #define ABSL_HAVE_STD_VARIANT 1
 #define ABSL_HAVE_STD_STRING_VIEW 1
+#endif
+
+#if defined(_MSC_VER)
+#define NOMINMAX
 #endif
 
 #endif  // ABSL_BASE_CONFIG_H_
