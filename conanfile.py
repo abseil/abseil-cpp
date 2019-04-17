a364a842b769a70be9be6a6cfb30c84f7055c0a0
Uilian Ries <uilianries@gmail.com>
2019-04-17 09:24:40 -0300

    Conan tag 20190416
    - As Conan exports the library list and Abseil can change the library
      names, we need to tag a specific version to list all correct
      libraries.
    - A new tag can be created monthly or when requested by issue
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -12,16 +12,20 @@ from conans.model.version import Version
 
 class AbseilConan(ConanFile):
     name = "abseil"
+    version = "2019041600"
     url = "https://github.com/abseil/abseil-cpp"
-    homepage = url
+    homepage = "https://abseil.io"
     author = "Abseil <abseil-io@googlegroups.com>"
     description = "Abseil Common Libraries (C++) from Google"
     license = "Apache-2.0"
     topics = ("conan", "abseil", "abseil-cpp", "google", "common-libraries")
-    exports = ["LICENSE"]
-    exports_sources = ["CMakeLists.txt", "CMake/*", "absl/*"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
+    scm = {
+         "type": "git",
+         "url": url,
+         "revision": "d902eb869bcfacc1bad14933ed9af4bed006d481"
+    }
 
     def configure(self):
         if self.settings.os == "Windows" and \
@@ -29,23 +33,77 @@ class AbseilConan(ConanFile):
            Version(self.settings.compiler.version.value) < "14":
             raise ConanInvalidConfiguration("Abseil does not support MSVC < 14")
 
-    def build(self):
-        tools.replace_in_file("CMakeLists.txt", "project(absl)", "project(absl)\ninclude(conanbuildinfo.cmake)\nconan_basic_setup()")
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTING"] = False
         cmake.configure()
+        return cmake
+
+    def build(self):
+        tools.replace_in_file("CMakeLists.txt", "project(absl)", """project(absl)
+                                                                    include(conanbuildinfo.cmake)
+                                                                    conan_basic_setup()""")
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
         self.copy("LICENSE", dst="licenses")
-        self.copy("*.h", dst="include", src=".")
-        self.copy("*.inc", dst="include", src=".")
-        self.copy("*.a", dst="lib", src=".", keep_path=False)
-        self.copy("*.lib", dst="lib", src=".", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
+        self.cpp_info.libs = [
+            "absl_spinlock_wait",
+            "absl_dynamic_annotations",
+            "absl_malloc_internal",
+            "absl_base",
+            "absl_throw_delegate",
+            "absl_scoped_set_env",
+            "absl_hashtablez_sampler",
+            "absl_raw_hash_set",
+            "absl_stacktrace",
+            "absl_symbolize",
+            "absl_examine_stack",
+            "absl_failure_signal_handler",
+            "absl_debugging_internal",
+            "absl_demangle_internal",
+            "absl_leak_check",
+            "absl_leak_check_disable",
+            "absl_hash",
+            "absl_city",
+            "absl_int128",
+            "absl_strings",
+            "absl_strings_internal",
+            "absl_str_format_internal",
+            "absl_graphcycles_internal",
+            "absl_synchronization",
+            "absl_time",
+            "absl_civil_time",
+            "absl_time_zone",
+            "absl_bad_any_cast_impl",
+            "absl_bad_optional_access",
+            "absl_bad_variant_access",
+            "absl_bad_variant_access",
+            "absl_hashtablez_sampler",
+            "absl_synchronization",
+            "absl_graphcycles_internal",
+            "absl_time",
+            "absl_civil_time",
+            "absl_time_zone",
+            "absl_examine_stack",
+            "absl_symbolize",
+            "absl_malloc_internal",
+            "absl_demangle_internal",
+            "absl_stacktrace",
+            "absl_debugging_internal",
+            "absl_leak_check",
+            "absl_strings",
+            "absl_int128",
+            "absl_strings_internal",
+            "absl_throw_delegate",
+            "absl_base",
+            "absl_spinlock_wait",
+            "absl_dynamic_annotations"
+        ]
         if self.settings.os == "Linux":
-            self.cpp_info.libs = ["-Wl,--start-group"]
-        self.cpp_info.libs.extend(tools.collect_libs(self))
-        if self.settings.os == "Linux":
-            self.cpp_info.libs.extend(["-Wl,--end-group", "pthread"])
+            self.cpp_info.libs.append("pthread")
