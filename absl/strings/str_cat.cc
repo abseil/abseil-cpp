6a9031be88df8f83fc29ee9acbce47001a8e11ae
Roman Gershman <romange@gmail.com>
2019-05-21 09:33:19 +0300

    Fix UB behavior when null ptr is passed to memcpy with size 0

--- absl/strings/str_cat.cc
+++ absl/strings/str_cat.cc
@@ -89,7 +89,7 @@ static char* Append(char* out, const AlphaNum& x) {
   // memcpy is allowed to overwrite arbitrary memory, so doing this after the
   // call would force an extra fetch of x.size().
   char* after = out + x.size();
-  memcpy(out, x.data(), x.size());
+  if (x.size()) memcpy(out, x.data(), x.size());
   return after;
 }
 
