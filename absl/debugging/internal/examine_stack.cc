6483a5201e2bf6af1a18c6e5fad14e8b2cd8bd66
Khem Raj <raj.khem@gmail.com>
2020-02-16 16:22:53 -0800

    Add RISCV support to GetProgramCounter()
    Identify PC register from signal context
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- absl/debugging/internal/examine_stack.cc
+++ absl/debugging/internal/examine_stack.cc
@@ -53,6 +53,8 @@ void* GetProgramCounter(void* vuc) {
     return reinterpret_cast<void*>(context->uc_mcontext.gp_regs[32]);
 #elif defined(__powerpc__)
     return reinterpret_cast<void*>(context->uc_mcontext.regs->nip);
+#elif defined(__riscv)
+    return reinterpret_cast<void*>(context->uc_mcontext.__gregs[REG_PC]);
 #elif defined(__s390__) && !defined(__s390x__)
     return reinterpret_cast<void*>(context->uc_mcontext.psw.addr & 0x7fffffff);
 #elif defined(__s390__) && defined(__s390x__)
