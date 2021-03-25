997aaf3a28308eba1b9156aa35ab7bca9688e9f6
Derek Mauro <761129+derekmauro@users.noreply.github.com>
2021-03-25 09:27:44 -0400

    Apply LTS transformations for 20210324 LTS branch (#920)
    * Apply LTS transformations for 20210324 LTS branch
    
    * Change the SOVERSION to make MacOS happy
    
    MacOS expects the first part of the SOVERSION to fit into 16 bits.

--- CMake/AbseilHelpers.cmake
+++ CMake/AbseilHelpers.cmake
@@ -263,7 +263,7 @@ Cflags: -I\${includedir}${PC_CFLAGS}\n")
     if(ABSL_ENABLE_INSTALL)
       set_target_properties(${_NAME} PROPERTIES
         OUTPUT_NAME "absl_${_NAME}"
-        SOVERSION 0
+        SOVERSION "2103.0.0"
       )
     endif()
   else()
