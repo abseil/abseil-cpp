63586dfd962f54b2c7060c5b076f0485a34ed0a0
Milad Fa <mfarazma@redhat.com>
2021-09-21 09:16:02 -0400

    make sysinfo change AIX specific

--- absl/base/internal/sysinfo.cc
+++ absl/base/internal/sysinfo.cc
@@ -131,11 +131,13 @@ static int GetNumCPUs() {
 #elif defined(_WIN32)
   const unsigned hardware_concurrency = Win32NumCPUs();
   return hardware_concurrency ? hardware_concurrency : 1;
+#elif defined (_AIX)
+  return sysconf(_SC_NPROCESSORS_ONLN);
 #else
   // Other possibilities:
   //  - Read /sys/devices/system/cpu/online and use cpumask_parse()
-  //  - sysconf(_SC_NPROCESSORS_ONLN)
-  return sysconf(_SC_NPROCESSORS_ONLN);
+  //  - sysconf(_SC_NPROCESSORS_ONLN); 
+  return std::thread::hardware_concurrency(); 
 #endif
 }
 
