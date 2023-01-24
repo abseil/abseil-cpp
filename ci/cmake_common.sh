534b5c4f355e8c8b591e0d248964bc7d301b4730
Derek Mauro <dmauro@google.com>
2023-01-24 13:56:39 -0500

    Apply LTS transformations for 20230124 LTS branch

--- ci/cmake_common.sh
+++ ci/cmake_common.sh
@@ -14,7 +14,7 @@
 
 # The commit of GoogleTest to be used in the CMake tests in this directory.
 # Keep this in sync with the commit in the WORKSPACE file.
-readonly ABSL_GOOGLETEST_COMMIT="934542165899c786cb5d8a710529c37184730183"
+readonly ABSL_GOOGLETEST_COMMIT="b796f7d44681514f58a683a3a71ff17c94edb0c1" # v1.13.0
 
 # Avoid depending on GitHub by looking for a cached copy of the commit first.
 if [[ -r "${KOKORO_GFILE_DIR:-}/distdir/${ABSL_GOOGLETEST_COMMIT}.zip" ]]; then
