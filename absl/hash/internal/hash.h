2fca64174fe9aefe9250bdab1586eb5d69649cca
Abseil Team <absl-team@google.com>
2023-11-22 11:07:57 -0800

    Avoid AbslHashValue for std::filesystem::path when <filesystem> is not included
    PiperOrigin-RevId: 584675181
    Change-Id: I78c9a29055327c84f1e78b8440221439839a041c

--- absl/hash/internal/hash.h
+++ absl/hash/internal/hash.h
@@ -589,6 +589,7 @@ H AbslHashValue(H hash_state, std::basic_string_view<Char> str) {
 #endif  // ABSL_HAVE_STD_STRING_VIEW
 
 #if defined(__cpp_lib_filesystem) && __cpp_lib_filesystem >= 201703L && \
+    !defined(_LIBCPP_HAS_NO_FILESYSTEM_LIBRARY) && \
     (!defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) ||        \
      __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 130000)
 
