16452e1418c1c2a8bcf4a99238e190ba901a20a6
Chris Mihelich <cmihelic@google.com>
2024-06-26 15:13:52 -0700

    Decoder for Rust-style Punycode encodings of bounded length.
    PiperOrigin-RevId: 647093624
    Change-Id: Ic76bfa4aa8fb616cb23095ce7bfa30c3812dcb21

--- CMake/AbseilDll.cmake
+++ CMake/AbseilDll.cmake
@@ -125,6 +125,8 @@ set(ABSL_INTERNAL_DLL_FILES
   "debugging/internal/address_is_readable.cc"
   "debugging/internal/address_is_readable.h"
   "debugging/internal/bounded_utf8_length_sequence.h"
+  "debugging/internal/decode_rust_punycode.cc"
+  "debugging/internal/decode_rust_punycode.h"
   "debugging/internal/demangle.cc"
   "debugging/internal/demangle.h"
   "debugging/internal/demangle_rust.cc"
