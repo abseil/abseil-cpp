a28c4b5827fefee822c34d42e0c67d6889942471
Abseil Team <absl-team@google.com>
2025-01-20 14:26:01 -0800

    Expand documentation for absl::raw_hash_set::erase to include idiom example of iterator post-increment.
    PiperOrigin-RevId: 717628872
    Change-Id: I0f2b585c03a61d96a6709c26e9a7290e0d9abbe5

--- absl/container/internal/raw_hash_set.h
+++ absl/container/internal/raw_hash_set.h
@@ -3209,17 +3209,33 @@ class raw_hash_set {
     return 1;
   }
 
-  // Erases the element pointed to by `it`.  Unlike `std::unordered_set::erase`,
-  // this method returns void to reduce algorithmic complexity to O(1).  The
-  // iterator is invalidated, so any increment should be done before calling
-  // erase.  In order to erase while iterating across a map, use the following
-  // idiom (which also works for some standard containers):
+  // Erases the element pointed to by `it`.
   //
-  // for (auto it = m.begin(), end = m.end(); it != end;) {
-  //   // `erase()` will invalidate `it`, so advance `it` first.
-  //   auto copy_it = it++;
-  //   if (<pred>) {
-  //     m.erase(copy_it);
+  // Unlike `std::unordered_set::erase`, this returns `void` to reduce
+  // algorithmic complexity to O(1). When erasing multiple elements, proper
+  // iterator management is crucial to avoid invalidation errors. The preferred
+  // method is `absl::erase_if`, which manages this complexity and simplifies
+  // the code. However, if specific requirements prevent using `absl::erase_if`
+  // (such as needing to perform extra operations on each element), the iterator
+  // must be manually advanced. Techniques like post-increment iteration with
+  // `erase` can ensure the iterator remains valid during the removal process.
+  //
+  // Example using absl::erase_if:
+  //
+  // absl::flat_hash_set<std::string> email_addresses = GetEmailAddresses();
+  // absl::erase_if(email_addresses,
+  //             [](const auto& a) { return !IsValidAddress(a); });
+  //
+  // Example using post-increment iteration:
+  //
+  // absl::flat_hash_set<std::string> email_addresses = GetEmailAddresses();
+  // for (auto it = email_addresses.begin(); it != email_addresses.end();) {
+  //   if (!IsValidAddress(*it)) {
+  //     process_element_being_erased(*it);
+  //     email_addresses.erase(it++);
+  //   } else {
+  //     process_element_being_kept(*it);
+  //     ++it;
   //   }
   // }
   void erase(const_iterator cit) { erase(cit.inner_); }
