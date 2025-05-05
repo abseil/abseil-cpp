7f281c1a280db1ed05e825b6970674bbfd39c65d
Evan Brown <ezb@google.com>
2025-05-05 15:17:45 -0700

    Add PolicyFunctions::soo_capacity() so that the compiler knows that soo_capacity() is always 0 or 1.
    Also, add AssertSoo()/AssertFullSoo() functions.
    
    PiperOrigin-RevId: 755092153
    Change-Id: I2eb3cfabaa34ae436af47e06b88531b2ed144e3a

--- absl/container/internal/raw_hash_set.h
+++ absl/container/internal/raw_hash_set.h
@@ -1640,7 +1640,7 @@ struct PolicyFunctions {
   uint32_t value_size;
   uint32_t slot_size;
   uint16_t slot_align;
-  uint8_t soo_capacity;
+  bool soo_enabled;
   bool is_hashtablez_eligible;
 
   // Returns the pointer to the hash function stored in the set.
@@ -1678,6 +1678,10 @@ struct PolicyFunctions {
       void* probed_storage,
       void (*encode_probed_element)(void* probed_storage, h2_t h2,
                                     size_t source_offset, size_t h1));
+
+  uint8_t soo_capacity() const {
+    return static_cast<uint8_t>(soo_enabled ? SooCapacity() : 0);
+  }
 };
 
 // Returns the maximum valid size for a table with 1-byte slots.
@@ -3609,8 +3613,7 @@ class raw_hash_set {
         static_cast<uint32_t>(sizeof(key_type)),
         static_cast<uint32_t>(sizeof(value_type)),
         static_cast<uint16_t>(sizeof(slot_type)),
-        static_cast<uint16_t>(alignof(slot_type)),
-        static_cast<uint8_t>(SooEnabled() ? SooCapacity() : 0),
+        static_cast<uint16_t>(alignof(slot_type)), SooEnabled(),
         ShouldSampleHashtablezInfoForAlloc<CharAlloc>(),
         // TODO(b/328722020): try to type erase
         // for standard layout and alignof(Hash) <= alignof(CommonFields).
