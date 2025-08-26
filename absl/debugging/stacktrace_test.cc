5d51d830b6dcf7afcc56b6f005a11e1a6aa2faf1
Abseil Team <absl-team@google.com>
2025-08-26 11:55:33 -0700

    stacktrace_test: avoid spoiling errno in the test signal handler.
    PiperOrigin-RevId: 799655439
    Change-Id: I5a06d39bb1188ec305c79ab76a8d0b8d51068dea

--- absl/debugging/stacktrace_test.cc
+++ absl/debugging/stacktrace_test.cc
@@ -27,7 +27,6 @@
 #include "gtest/gtest.h"
 #include "absl/base/attributes.h"
 #include "absl/base/config.h"
-#include "absl/base/internal/errno_saver.h"
 #include "absl/base/optimization.h"
 #include "absl/types/span.h"
 
@@ -415,7 +414,6 @@ const void* g_return_address = nullptr;
 bool g_sigusr2_raised = false;
 
 void SigUsr2Handler(int, siginfo_t*, void* uc) {
-  absl::base_internal::ErrnoSaver errno_saver;
   // Many platforms don't support this by default.
   bool support_is_expected = false;
   constexpr int kMaxStackDepth = 64;
