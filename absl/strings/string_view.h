fdb6e72c75f24009aefb8371f45bc6d1a51c3150
Jesse Rosenstock <jmr@google.com>
2025-10-20 07:12:54 -0700

    string_view: Fix include order
    PiperOrigin-RevId: 821635144
    Change-Id: I5e3e8457482c926aabf9ee016f28956f05064ed0

--- absl/strings/string_view.h
+++ absl/strings/string_view.h
@@ -39,10 +39,10 @@
 #include <type_traits>
 
 #include "absl/base/attributes.h"
-#include "absl/base/nullability.h"
 #include "absl/base/config.h"
 #include "absl/base/internal/throw_delegate.h"
 #include "absl/base/macros.h"
+#include "absl/base/nullability.h"
 #include "absl/base/optimization.h"
 #include "absl/base/port.h"
 
