be96802d5d5c24a75a5a56d455ddcee8b709a53b
Mike Kruskal <mkruskal@google.com>
2026-07-10 15:47:43 -0700

    Fix flaky hash test
    PiperOrigin-RevId: 945928438
    Change-Id: Ife2cc0d06e662cad54bcd2d2ddfd19004623c58a

--- absl/hash/hash_test.cc
+++ absl/hash/hash_test.cc
@@ -192,7 +192,7 @@ TEST(HashValueTest, PointerAlignment) {
     constexpr size_t kMask = (1 << (kLog2NumValues + 7)) - 1;
     size_t stuck_bits = (~bits_or | bits_and) & kMask;
     int stuck_bit_count = absl::popcount(stuck_bits);
-    size_t max_stuck_bits = 5;
+    size_t max_stuck_bits = 8;
     EXPECT_LE(stuck_bit_count, max_stuck_bits)
         << "0x" << std::hex << stuck_bits;
 
