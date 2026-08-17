bef04412e565236712ee7272fff9e463ab90d61d
Javid Khan <dxbjavid@gmail.com>
2026-08-17 22:36:04 +0530

    guard SnprintF return against int overflow

--- absl/strings/internal/str_format/bind.cc
+++ absl/strings/internal/str_format/bind.cc
@@ -267,6 +267,10 @@ int SnprintF(char* output, size_t size, const UntypedFormatSpecImpl format,
   }
   size_t total = sink.total_written();
   if (size) output[std::min(total, size - 1)] = 0;
+  if (total > static_cast<size_t>(std::numeric_limits<int>::max())) {
+    errno = EFBIG;
+    return -1;
+  }
   return static_cast<int>(total);
 }
 
