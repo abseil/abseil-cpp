e49cd4a52313f65edbde0a7b9e3ea45c1d377c2c
Abseil Team <absl-team@google.com>
2026-08-17 10:16:44 -0700

    Remove pre-C++17 workaround in absl/types/internal/span.h
    PiperOrigin-RevId: 966043277
    Change-Id: I7de663330fdde466d3372c883d5fba571b8541a4

--- absl/types/internal/span.h
+++ absl/types/internal/span.h
@@ -39,12 +39,6 @@ constexpr auto GetDataImpl(C& c, char) noexcept  // NOLINT(runtime/references)
   return c.data();
 }
 
-// Before C++17, std::string::data returns a const char* in all cases.
-inline char* GetDataImpl(std::string& s,  // NOLINT(runtime/references)
-                         int) noexcept {
-  return &s[0];
-}
-
 template <typename C>
 constexpr auto GetData(C& c) noexcept  // NOLINT(runtime/references)
     -> decltype(GetDataImpl(c, 0)) {
