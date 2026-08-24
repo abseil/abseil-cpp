6b94d9a9e48a939a24df87e9e2e5612c2141f803
Abseil Team <absl-team@google.com>
2026-08-24 10:17:47 -0700

    Add missing #includes
    PiperOrigin-RevId: 969929911
    Change-Id: I48575a9b3407ba71f2b6314b3024bee7c2a5cc12

--- absl/profiling/internal/profile_builder.cc
+++ absl/profiling/internal/profile_builder.cc
@@ -28,6 +28,7 @@
 #include "absl/strings/escaping.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 
 #ifdef __linux__
